c336368b9687d5ac8e6f7f096f53db4493fba8b7
XenderMd <denis.stavilamd@gmail.com>
2022-01-20 14:55:57 +0100

    Logging application messages

--- src/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ src/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -14,18 +15,25 @@ namespace OdeToFood.Pages.Restaurants
     {
         private readonly IConfiguration config;
         private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
+
         public string Message{get; set;}
         public IEnumerable<Restaurant> Restaurants { get; set; }
         
         [BindProperty(SupportsGet = true)]
         public string SearchTerm {get; set;}
 
-        public ListModel(IConfiguration config, IRestaurantData restaurantData){
+        public ListModel(IConfiguration config, 
+                         IRestaurantData restaurantData, 
+                         ILogger<ListModel> logger){
+
             this.config = config;
             this.restaurantData = restaurantData;
+            this.logger = logger;
         }
         public void OnGet()
-        {   
+        {
+            logger.LogInformation("executing List Model");
             Message = config["Message"];
             Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
             // Console.WriteLine($"Search term is {SearchTerm}");
